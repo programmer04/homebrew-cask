fbdea35bab9a476a4655ad3e293fab3b55c6ba66
Markus Reiter <me@reitermark.us>
2020-11-24 19:42:21 +0100

    Try reproducing “bom contains no files, nothing copied” error.

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,6 +1,6 @@
 cask "sizzy" do
   version "0.44.0"
-  sha256 "171e6befab2c7ad81f65ecae76a3df65561605ba66172c8a6f46a774d39b5de7"
+  sha256 :no_check
 
   url "https://sizzy.co/get-app"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X"
